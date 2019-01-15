aee530df29c1c2f88e8196c49cff55aa3a7df395
spaceone <space@wechall.net>
2019-01-15 19:55:48 +0100

    Implement filesystem commands

--- spacebot/plugins/filesystem.py
+++ spacebot/plugins/filesystem.py
@@ -1,13 +1,60 @@
 # -*- coding: utf-8 -*-
 
+import os
+
 from spacebot.plugins import Command, ArgumentInfos
+
+
+class Directory(int):
+	pass
+
+
+class File(int):
+	pass
+
+
+class Executable(File):
+	pass
+
+
+FS = {
+	'/': Directory(0),
+	'/bin/': Directory(1),
+	'/bin/sh': Executable(14),
+	'/bin/true': Executable(15),
+	'/bin/false': Executable(16),
+	'/bin/bash': Executable(17),
+	'/bin/ls': Executable(18),
+	'/bin/cat': Executable(19),
+	'/bin/find': Executable(20),
+	'/bin/cd': Executable(21),
+	'/etc/': Directory(2),
+	'/etc/passwd': File(11),
+	'/etc/shadow': File(12),
+	'/var/': Directory(4),
+	'/var/log/': Directory(5),
+	'/var/log/apache/': Directory(6),
+	'/var/log/apache/access.log': File(46),
+	'/sbin/': Directory(9),
+	'/home/': Directory(22),
+	'/home/spaceone/': Directory(50),
+	'/home/gizmore/.bash_history': File(65),
+	'/home/gizmore/': Directory(39),
+}
+FS = dict((inode, name) for name, inode in FS.iteritems())
+ELF = r'\x7fELF\x02\x01\x01\x00'
+
+FILES = {
+	11: 'root:x:0:0:root:/:/bin/false\nmailer:x:100:100:mailer:/var:/bin/false\ngizmore:x:Gizmore:/home/gizmore:/bin/sh\nspaceone:x:SpaceOne:/home/spaceone:/bin/bash\n',
+	12: 'Permission denied',
+	46: '',
+	65: 'sudo su\ncd ~/git\ncd www/challenges/\nfind\ncat dloser/brownos/solution.php\nexit\nfirefox\n',
+	14: ELF, 15: ELF, 16: ELF, 17: ELF, 18: ELF, 19: ELF, 20: ELF, 21: ELF,
+}
 
 
 class LS(Command):
 	'''list directory contents'''
-
-	threaded = True
-	exceptions = ()
 
 	def register(self):
 		parser = super(LS, self).register()
@@ -26,7 +73,7 @@ class LS(Command):
 		def _colorify(file_or_dir):
 			if args.color in ('auto', 'always') and (file_or_dir.endswith('/') or file_or_dir in ('.', '..')):
 				file_or_dir = '\x0302%s\x03' % (file_or_dir,)
-			elif args.color in ('auto', 'always') and file_or_dir in ('false', 'sh', 'sudo'):
+			elif args.color in ('auto', 'always') and isinstance(file_or_dir, Executable):
 				file_or_dir = '\x0309%s\x03' % (file_or_dir,)
 			return file_or_dir
 
@@ -45,9 +92,6 @@ class LS(Command):
 
 class Find(Command):
 	'''search for files in a directory hierarchy'''
-
-	threaded = True
-	exceptions = ()
 
 	def register(self):
 		parser = super(Find, self).register()
@@ -70,12 +114,65 @@ class Find(Command):
 class Cat(Command):
 	'''concatenate files and print on the standard output'''
 
-	threaded = True
-	exceptions = ()
-
 	def register(self):
 		parser = super(Cat, self).register()
 		parser.add_argument('filename')
 
 	def __call__(self, args):
 		return cat(args.filename).splitlines()
+
+
+def find(filename):
+	return dict((name, inode) for inode, name in FS.iteritems())
+
+
+def cat(filename):
+	inode = ls(filename).get('.')
+	if inode is None:
+		return 'No such file or directory'
+	return FILES.get(inode, 'Is a directory')
+
+
+def ls(directory='/'):
+	parent_inode = 0
+	root = _ls(parent_inode)
+	is_file = False
+	for segment in directory.split('/'):
+		if is_file:
+			return _ls(256)  # will cause a "not a directory" error
+		if not segment or segment in ('.',):
+			continue
+		if segment not in root:
+			if segment + '/' in root:
+				segment += '/'
+			else:
+				print 'root', root, segment
+				root = {segment: Directory(256)}  # will cause a "no directory" error
+		if isinstance(root[segment], Directory):
+			root = _ls(root[segment])
+			root['..'] = Directory(parent_inode)
+			parent_inode = root['.']
+		else:
+			is_file = True
+	filename = os.path.basename(directory)
+	if isinstance(root.get(filename), File):
+		root = {'.': root[filename], filename: root[filename]}
+	return root
+
+
+def _ls(inode):
+	if inode in FILES:
+		raise ArgumentInfos('No such file or directory')
+	directory = FS.get(inode)
+	if not directory:
+		raise ArgumentInfos('Not a directory')
+	result = dict((get_name(_inode), _inode) for _inode, name in FS.items() if name.startswith(directory) and name != directory and '/' not in name[len(directory):].rstrip('/'))
+	result['.'] = inode
+	return result
+
+
+def get_name(inode):
+	name = FS.get(inode)
+	if not name:
+		raise ArgumentInfos('No such file or directory')
+	return os.path.basename(name) or os.path.basename(os.path.dirname(name)) or os.path.dirname(name)
