4f6deedc4ceed78d756d47f4198a0a18a8182f9b
spaceone <space@wechall.net>
2019-02-19 22:05:23 +0100

    Ignore tehbot

--- spacebot/plugins/wechall.py
+++ spacebot/plugins/wechall.py
@@ -9,21 +9,23 @@ from circuits.protocols.irc import PRIVMSG
 
 class WeChallCommands(BaseComponent):
 
-	WC = re.compile(r'''^ok(?:ay)?\s+(?:tehbot|spacebot),?\s*(?:has|did)\s+(?P<who>\w+)\s+solved?\s+(?P<chall>\w[\s\w]*?|"[^"]+"|'[^']+')(?:\s+on\s+(?P<site>\w[\s\w]*?|"[^"]+"|'[^']+'))?\s*\??$''', re.I)
+	WC = re.compile(r'''^(?:ok|okay|hey)\s+(?P<bot>\w*),?\s*(?:has|did)\s+(?P<who>\w+)\s+solved?\s+(?P<chall>\w[\s\w]*?|"[^"]+"|'[^']+')(?:\s+on\s+(?P<site>\w[\s\w]*?|"[^"]+"|'[^']+'))?\s*\??$''', re.I)
 
 	@handler('privmsg', channel='*', priority=0.6)
 	def _okay_privmsg(self, event, source, target, message):
+		match = self.WC.search(message)
+		if not match:
+			return
+
 		server = self.parent.parent.servers[event.channels[0]]
 		dest = source[0] if target == server.nick else target
-		match = self.WC.search(message)
-		if match:
-			event.stop()
-			user = match.group(1)
-			chall = match.group(2)
-			site = match.group(3) or 'wc'
-			if site.lower() in ('wc', 'wechall'):
-				part = solvers(chall, user=user)
-				self.fire(PRIVMSG(dest, part), server.channel)
+		event.stop()
+		user = match.group(1)
+		chall = match.group(2)
+		site = match.group(3) or 'wc'
+		if site.lower() in ('wc', 'wechall') and match.group('bot') != 'tehbot':
+			part = solvers(chall, user=user)
+			self.fire(PRIVMSG(dest, part), server.channel)
 
 
 class WeChall(Command):
